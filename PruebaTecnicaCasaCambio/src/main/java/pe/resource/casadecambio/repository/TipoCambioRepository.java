6e91fa65ad4c6b496028c58d38432db734e94d97
Christopher Peñaloza <139002625+test-railway@users.noreply.github.com>
2025-01-25 21:34:57 -0500

    Delete PruebaTecnicaCasaCambio directory

--- PruebaTecnicaCasaCambio/src/main/java/pe/resource/casadecambio/repository/TipoCambioRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.resource.casadecambio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.resource.casadecambio.entity.TipoCambio;
-
-@Repository
-public interface TipoCambioRepository extends JpaRepository<TipoCambio, Long>{
-}
--- PruebaTecnicaCasaCambio/src/main/resources/import.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS moneda (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    codigo VARCHAR(255) NOT NULL,
-    nombre VARCHAR(255) NOT NULL
-    );
-
-INSERT INTO moneda (codigo, nombre) VALUES ('PEN', 'Sol Peruano');
-INSERT INTO moneda (codigo, nombre) VALUES ('USD', 'Dólar Estadounidense');
-INSERT INTO moneda (codigo, nombre) VALUES ('EUR', 'Euro');
